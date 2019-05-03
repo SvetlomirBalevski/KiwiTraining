9eb5055c1d310c4143cd1ff66b2fc0d449f32fc8
s.balevski@gmail.com <s.balevski@gmail.com>
2019-05-03 23:08:12 +0300

    Create first file from Django tutorial

---
+++ Django/django-tutorial/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/desertfox/Desktop/PythonCode/KiwiCourse/6/django-tutorial/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
