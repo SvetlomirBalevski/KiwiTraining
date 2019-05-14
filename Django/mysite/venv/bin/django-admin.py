cc2323a5e3d75e690320670e7fb257b8e5f47a26
s.balevski@gmail.com <s.balevski@gmail.com>
2019-05-14 22:20:01 +0300

    Added 2 models

---
+++ Django/mysite/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/desertfox/Desktop/PythonCode/KiwiCourse/Django/mysite/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
